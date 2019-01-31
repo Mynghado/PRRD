032c20512076df9eef1fef349f5a1625d030b58b
Mynghado <kaamec@gmail.com>
2019-01-31 23:40:52 +0100

    deleted HelloWorld.vue

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,6 @@
   <div id="app">
       <NavBar />
       <router-view/>
-
-      <!--<img alt="Vue logo" src="./assets/logo.png">
-      <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-
       <Footer />
   </div>
 </template>
