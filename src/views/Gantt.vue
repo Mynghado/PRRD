7c099769e1cd7715dad92eb6cbc34813cdbcb023
Hakan Gunes <hakangunes80@gmail.com>
2019-02-17 12:46:37 +0100

    Implémentation de Gantt dhtmlX

--- src/views/Gantt.vue
+++ src/views/Gantt.vue
@@ -1,38 +1,100 @@
 <template>
-<div id="SignIn">
-  <v-container>
-    <v-layout row>
-        <!--<v-flex xs12 sm6 offset-sm3>-->
-        <!--<v-flex xs12 sm6 offset-sm3 md6 offset-md3>-->
-        <v-flex xs12 sm8 offset-sm2 md6 offset-md3 lg4 offset-lg4 >
-            <v-card>
-                <v-card-title>
-                    <v-container class="text-xs-center">
-                        <h2>Connexion</h2>
-                    </v-container>
-                </v-card-title>
-                <v-card-text>
-                    <form>
-                        <v-flex xs12>
-                            <v-text-field v-model="project_team" label="Identifiant" prepend-inner-icon="person" solo outline></v-text-field>
-                            <v-text-field v-model="password" label="Mot de passe" prepend-inner-icon="lock" solo outline></v-text-field>
-                        </v-flex>
-                        <div class="text-xs-center">
-                            <v-btn round color="primary" dark>Se connecter</v-btn>
-                        </div>
-                    </form>
-                </v-card-text>
-            </v-card>
-        </v-flex>
-    </v-layout>
-  </v-container>
-</div>
+
+  
+    
+        
+           
+  
+  <div class="container">
+    <div class="right-container">
+      <div class="gantt-selected-info">
+        <div v-if="selectedTask">
+          <h2>{{selectedTask.text}}</h2>
+          <span><b>ID: </b>{{selectedTask.id}}</span><br/>
+          <span><b>Progress: </b>{{selectedTask.progress|toPercent}}%</span><br/>
+          <span><b>Start Date: </b>{{selectedTask.start_date|niceDate}}</span><br/>
+          <span><b>End Date: </b>{{selectedTask.end_date|niceDate}}</span><br/>
+        </div>
+        <div v-else class="select-task-prompt">
+          <h2>Click any task</h2>
+        </div>
+      </div>
+      <ul class="gantt-messages">
+        <li class="gantt-message" v-for="message in messages">{{message}}</li>
+      </ul>
+    </div>
+    <gantt class="left-container" :tasks="tasks" @task-updated="logTaskUpdate" @link-updated="logLinkUpdate" @task-selected="selectTask"></gantt>
+  </div>
+                   
+            
+        
+    
+  
+
 </template>
 
 <script>
+
+import Gantt from './Gantt2.vue'
+
 export default {
-    name: 'SignIn'
+  name: 'test',
+  components: {Gantt},
+  data () {
+    return {
+      tasks: {
+        data: [
+          {id: 1, text: 'Task #1', start_date: '15-04-2017', duration: 3, progress: 0.6},
+          {id: 2, text: 'Task #2', start_date: '18-04-2017', duration: 3, progress: 0.4}
+        ],
+        links: [
+          {id: 1, source: 1, target: 2, type: '0'}
+        ]
+      },
+    selectedTask: null,
+      messages: []
+    }
+  },
+  filters: {
+    toPercent (val) {
+      if(!val) return '0'
+      return Math.round((+val) * 100)
+    },
+    niceDate (obj){
+      return `${obj.getFullYear()} / ${obj.getMonth() + 1} / ${obj.getDate()}`
+    }
+  },
+  methods: {
+    selectTask (task) {
+      this.selectedTask = task
+    },
+  
+    addMessage (message) {
+      this.messages.unshift(message)
+      if(this.messages.length > 40) {
+        this.messages.pop()
+      }
+    },
+
+    logTaskUpdate (id, mode, task) {
+      let text = (task && task.text ? ` (${task.text})`: '')
+      let message = `Task ${mode}: ${id} ${text}`
+      this.addMessage(message)
+    },
+
+    logLinkUpdate (id, mode, link) {
+      let message = `Link ${mode}: ${id}`
+      if(link){
+        message += ` ( source: ${link.source}, target: ${link.target} )`
+      }
+      this.addMessage(message)
+    }
+  }
 }
+
+
+
+
 </script>
 
 <style>
