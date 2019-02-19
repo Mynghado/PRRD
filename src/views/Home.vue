37145a1d9b0f5f7f527e279aa875358753919039
viperee <just.b.nd.l@gmail.com>
2019-02-19 22:21:33 +0100

    PRRD-23 Page home in progress

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,38 @@
 <template>
   <v-container>
-    <!--<v-layout align-start justify-center row>
+    <v-carousel>
+      <v-carousel-item
+        src="https://i0.wp.com/www.stjust34.com/wp-content/uploads/2016/07/bibliotheque.jpg?fit=1030%2C687"
+      ></v-carousel-item>
+      <v-carousel-item
+        src="https://i0.wp.com/www.stjust34.com/wp-content/uploads/2016/07/bibliotheque.jpg?fit=1030%2C687"
+      ></v-carousel-item>
+    </v-carousel>
+    <v-layout align-start justify-center row>
       <h1>Bienvenue</h1>
-    </v-layout>-->
-    <v-layout row wrap>
-      <v-flex tag="h1" class="headline">Site actuellement en construction</v-flex>
-      <v-flex d-flex xs12 order-xs5>
-        <v-layout column>
-          <v-flex>
-            <v-card flat>
-              <v-card-text>{{ lorem }}</v-card-text>
-            </v-card>
-          </v-flex>
-          <v-flex>
-            <v-card flat>
-              <v-card-text>{{ lorem }}</v-card-text>
-            </v-card>
-          </v-flex>
-        </v-layout>
+    </v-layout>
+    <v-layout row>
+      
+      <v-flex xs4 ma-5>
+        <v-card>
+          <v-img
+            src="https://i0.wp.com/www.stjust34.com/wp-content/uploads/2016/07/bibliotheque.jpg?fit=1030%2C687"
+          ></v-img>
+        </v-card>
+      </v-flex>
+      <v-flex xs4 ma-5>
+        <v-card>
+          <v-img
+            src="https://i0.wp.com/www.stjust34.com/wp-content/uploads/2016/07/bibliotheque.jpg?fit=1030%2C687"
+          ></v-img>
+        </v-card>
+      </v-flex>
+      <v-flex xs4 ma-5>
+        <v-card>
+          <v-img
+            src="https://i0.wp.com/www.stjust34.com/wp-content/uploads/2016/07/bibliotheque.jpg?fit=1030%2C687"
+          ></v-img>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -25,15 +40,15 @@
 
 <script>
 export default {
-    name: 'Home',
-    data () {
-        return {
-            lorem: 'Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alienum argumentum ius ad. Pri eu justo aeque torquatos.'
-        }
-    }
-}
+  name: "Home",
+  data() {
+    return {
+      lorem:
+        "Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alienum argumentum ius ad. Pri eu justo aeque torquatos."
+    };
+  }
+};
 </script>
 
 <style>
-
 </style>
