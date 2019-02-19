95d73e5770bea4d8abfda72453aa1454a02255df
Mynghado <kaamec@gmail.com>
2019-02-19 03:01:41 +0100

    page in construction

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,26 @@
 <template>
-<div id="Home">
-    <h1>Home content</h1>
-</div>
+  <v-container>
+    <!--<v-layout align-start justify-center row>
+      <h1>Bienvenue</h1>
+    </v-layout>-->
+        <v-layout row wrap>
+      <v-flex tag="h1" class="headline">Site actuellement en construction</v-flex>
+      <v-flex d-flex xs12 order-xs5>
+        <v-layout column>
+          <v-flex>
+            <v-card flat>
+              <v-card-text>{{ lorem }}</v-card-text>
+            </v-card>
+          </v-flex>
+          <v-flex>
+            <v-card flat>
+              <v-card-text>{{ lorem }}</v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -9,7 +28,7 @@ export default {
     name: 'Home',
     data () {
         return {
-
+            lorem: 'Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alienum argumentum ius ad. Pri eu justo aeque torquatos.'
         }
     }
 }
